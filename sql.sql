51eafc36338a879f5794fc1c0b8e98c7dee6997f
秦聪 <1309740551@qq.com>
2015-08-30 10:32:27 +0800

    sql语句

--- sql.sql
+++ sql.sql
@@ -1,1 +1,15 @@
-ji
+create database zhaodedao;
+
+
+create table goods_message(
+	gid int not null auto_increment ,
+	is_lost int,y
+	goods_name varchar(50) not null,
+	photodir varchar(50),
+	location varchar(100),
+	gettime varchar(50),
+	description text,
+	user_email varchar(50),
+	user_phone varchar(50),
+	primary key(gid)
+);
